18db6a3a5b32a3b186885bdbdb18e9b44e8fbb8b
luana2u5 <luana2u5@gmail.com>
2017-10-17 14:51:49 -0200

    Update Cliente.java

--- src/java/Leilao/Cliente.java
+++ src/java/Leilao/Cliente.java
@@ -9,7 +9,7 @@ package Leilao;
  *
  * @author Luana
  */
-import java.io.IOException;
+     import java.io.IOException;
      import java.io.PrintStream;
      import java.net.Socket;
      import java.net.UnknownHostException;
@@ -18,8 +18,11 @@ import java.io.IOException;
      public class Cliente {
          public static void main(String[] args)
                          throws UnknownHostException, IOException {
-             Socket cliente = new Socket("127.0.0.1", 12345);
-             System.out.println("O cliente se conectou ao servidor!");
+             
+             Integer idusuario; 
+             
+            // Socket cliente = new Socket("127.0.0.1", 12345);
+            // System.out.println("O cliente se conectou ao servidor!");
 
              Scanner teclado = new Scanner(System.in);
              PrintStream saida = new PrintStream(cliente.getOutputStream());
